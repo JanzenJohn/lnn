0d06a95fb24a185f150c93062245740d246390d4
PotatoMaaan <60669730+PotatoMaaan@users.noreply.github.com>
2023-06-06 22:33:57 +0200

    unfegen2

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ struct Args {
     source: PathBuf,
     /// The target file/directory (defaults to .)
     target: Option<PathBuf>,
+    /// Skip unknown files
+    #[arg(short, long)]
+    skip_unknown: bool,
 }
 
 fn main() {
@@ -17,28 +20,35 @@ fn main() {
     let source_dir = args.source;
     let target_dir = args.target.unwrap_or(".".into());
 
+    if !source_dir.is_dir() {
+        eprintln!("Could not read source directory {}", source_dir.display());
+        return;
+    }
+
     if !target_dir.exists() {
         fs::create_dir(&target_dir).expect("Failed to create target directory")
     }
 
-    copy_dir(&source_dir, &target_dir)
+    copy_dir(&source_dir, &target_dir, args.skip_unknown)
         .map_err(|e| println!("Linking failed: {}", e))
         .unwrap();
 }
 
-fn copy_dir(source: &PathBuf, target: &PathBuf) -> Result<(), std::io::Error> {
-    for entry in fs::read_dir(source).unwrap() {
-        let entry = entry.unwrap();
+fn copy_dir(source: &PathBuf, target: &PathBuf, skip_unknown: bool) -> Result<(), std::io::Error> {
+    for entry in fs::read_dir(source)? {
+        let entry = entry?;
         let target = target.join(entry.file_name());
 
         if entry.path().is_dir() {
             fs::create_dir(&target)?;
-            copy_dir(&entry.path(), &target)?;
+            copy_dir(&entry.path(), &target, skip_unknown)?;
         } else if entry.path().is_file() {
             println!("linking {} -> {}", source.display(), target.display());
             fs::hard_link(entry.path(), target)?;
-        } else {
+        } else if skip_unknown {
             println!("Unkown file, skipping: {}", entry.path().display());
+        } else {
+            return Err(std::io::ErrorKind::Other.into());
         }
     }
     Ok(())
